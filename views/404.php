6911df96c333d6f43855012573cda341256c9068
pierre <maxence@gmail.com>
2023-05-15 16:05:27 +0200

    Mise en place frontend + finalisation accueil et page 404

--- views/404.php
+++ views/404.php
@@ -1,16 +1,43 @@
 <!doctype html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Erreur 404 - Wikifruit</title>
+    <!-- Inclusion du contenu du fichier header.php -->
+    <?php include VIEWS_DIR . '/partials/header.php';?>
+
 </head>
 <body>
 
-                <h1>Erreur 404 : introuvable</h1>
+    <!-- Inclusion du contenu du fichier menu.php -->
+    <?php include VIEWS_DIR . '/partials/menu.php'; ?>
 
 
+    <div class="container">
+
+        <!--Titre h1-->
+        <div class="row my-5">
+            <div class="col-12">
+                <h1 class=text-center>Erreur 404 - Wikifruit</h1>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="col-12">
+
+                    <p class="alert alert-warning fw-bold text-center">Désolé cette page n'existe pas !</p>
+
+                <div class="text-center">
+                    <img src="<?= PUBLIC_PATH ?>/images/404.png" alt="">
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+           <!-- Inclusion du contenu du fichier footer.php -->
+    <?php include VIEWS_DIR . '/partials/footer.php'; ?>
 </body>
 </html>
--- views/home.php
+++ views/home.php
@@ -1,17 +1,45 @@
 <!doctype html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Accueil - Wikifruit</title>
+    <!-- Inclusion du contenu du fichier header.php -->
+    <?php include VIEWS_DIR . '/partials/header.php';?>
+
 </head>
 <body>
 
-                <h1>Page d'accueil</h1>
+    <!-- Inclusion du contenu du fichier menu.php -->
+    <?php include VIEWS_DIR . '/partials/menu.php'; ?>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolore, doloremque dolores dolorum ducimus earum, facilis iste minus quaerat quas repellat, sit tenetur totam unde ut. Quia quibusdam sed ut.</p>
 
+    <div class="container">
+
+        <!--Titre h1-->
+        <div class="row my-5">
+            <div class="col-12">
+                <h1 class=text-center>Accueil - Wikifruit</h1>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="col-12 mb-5">
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet at aut eaque eveniet ex exercitationem facere illum ipsum, iure labore, laboriosam laudantium maxime pariatur quaerat quidem quos sequi sit soluta! Aliquam autem consequatur, dolore eaque earum error est fugit ipsam ipsum minus non odio odit porro quis ratione, rem repellendus repudiandae saepe similique sint sit sunt totam veniam, vero vitae.</p>
+
+                <div class="d-flex justify-content-around flex-wrap home-fruits">
+                    <img src="<?= PUBLIC_PATH ?>/images/banane.jpg" alt="">
+                    <img src="<?= PUBLIC_PATH ?>/images/kiwi.jpg" alt="">
+                    <img src="<?= PUBLIC_PATH ?>/images/orange.jpg" alt="">
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+           <!-- Inclusion du contenu du fichier footer.php -->
+    <?php include VIEWS_DIR . '/partials/footer.php'; ?>
 </body>
 </html>
